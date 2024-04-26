dc0d4bad0bdefc7d4753ff45ef0613eb4e7e7ae2
Dima <Dmitriy.sai2013@gmail.com>
2024-04-26 11:17:04 +0300

    Add privacy manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .target(name: "SideMenu", path: "Pod/Classes")
+        .target(name: "SideMenu", path: "Pod/Classes", resources: [.copy("PrivacyInfo.xcprivacy")])
     ],
     swiftLanguageVersions: [.v4_2, .v5]
 )
